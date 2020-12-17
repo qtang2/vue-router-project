9b3e01b68af338f669dec69f97c9d67eb2a1fceb
Qian Tang <q.tang6@student.unimelb.edu.au>
2020-12-17 16:18:43 +1100

    fetch data from a fake server, which is created by using json-server package

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, aliquam tempore dolorem quas deserunt iste nam impedit. Ipsum, ab quidem. Laborum qui eius voluptate veniam ab mollitia officia velit aperiam. Eaque quia explicabo nobis voluptates. Eligendi eum aliquam amet consequuntur esse optio. Vel sapiente, autem cupiditate sequi voluptate iure laborum ipsum porro. Laudantium ullam voluptatibus ut vero sequi. Nostrum iure consequuntur perspiciatis quisquam enim commodi ducimus consectetur, sint nesciunt. Asperiores itaque dolor impedit commodi unde quia qui totam aliquid natus voluptatum laboriosam aut doloribus incidunt ex quam, excepturi suscipit quidem assumenda numquam corporis, repellendus quod vitae molestias. Eos, similique possimus?</p>
   </div>
 </template>
+
+
+<script>
+
+export default {
+  name: 'About',
+  components: {
+    
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste excepturi minima eum delectus eos possimus ipsam illum nobis tenetur eligendi, provident officia reiciendis voluptate est ut impedit aperiam quidem illo aspernatur eveniet unde praesentium maxime laboriosam. Eaque nostrum dolores ipsam maiores omnis, ex quisquam ea maxime mollitia assumenda expedita ratione iusto aliquam eius numquam asperiores fugiat voluptate illo ut laborum incidunt repellat? Quo accusantium deleniti iusto fugit similique vitae impedit praesentium qui veniam repellendus, quis animi non enim nam debitis minus numquam corporis assumenda temporibus, eaque neque voluptas excepturi! Rerum perferendis ipsum quae minus quibusdam harum nostrum beatae repellat consequatur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste excepturi minima eum delectus eos possimus ipsam illum nobis tenetur eligendi, provident officia reiciendis voluptate est ut impedit aperiam quidem illo aspernatur eveniet unde praesentium maxime laboriosam. Eaque nostrum dolores ipsam maiores omnis, ex quisquam ea maxime mollitia assumenda expedita ratione iusto aliquam eius numquam asperiores fugiat voluptate illo ut laborum incidunt repellat? Quo accusantium deleniti iusto fugit similique vitae impedit praesentium qui veniam repellendus, quis animi non enim nam debitis minus numquam corporis assumenda temporibus, eaque neque voluptas excepturi! Rerum perferendis ipsum quae minus quibusdam harum nostrum beatae repellat consequatur?</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
